152c43fcccccee979ad9b3f83e4ef790ceeb38f2
TimilsinaBimal <timilsina.bml05@gmail.com>
2020-07-10 22:31:02 +0545

    Stack Implementation of String Reverser

--- stacks/string_reverser.py
+++ stacks/string_reverser.py
@@ -1,9 +1,23 @@
-my_string = input("Enter any string you want to reverse: ")
-arr = []
-for i in my_string:
-    arr.append(i)
-reversed_string = ''
-while(arr):
-    reversed_string+= arr.pop()
+from stack import Stack
 
-print(f"The reversed string is: {reversed_string}")
+
+def reverse_string(my_string):
+    """Return the input string in reverse order"""
+    stack = Stack()
+    for i in my_string:
+        stack.push(i)
+    reversed_string = ''
+    while(not stack.is_empty()):
+        reversed_string += stack.peek()
+        stack.pop()
+    return reversed_string
+
+
+def main():
+    my_string = input("Enter any string you want to reverse: ")
+    reversed_string = reverse_string(my_string)
+    print(f"The reversed string is: {reversed_string}")
+
+
+if __name__ == "__main__":
+    main()
