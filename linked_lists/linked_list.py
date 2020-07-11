cb9b92b1359d2832cca88e038525eaea6b325b10
TimilsinaBimal <timilsina.bml05@gmail.com>
2020-07-11 12:51:50 +0545

    FIX: LinkedList- reverse_list

--- linked_lists/linked_list.py
+++ linked_lists/linked_list.py
@@ -10,13 +10,22 @@ class LinkedList:
         self.last = None
         self._size = 0
 
+    def __str__(self):
+        return str(self.toArray())
+
     def isEmpty(self):
+        """
+        Checks if the linked list is empty or not.
+        """
         if self.first is None:
             return True
         else:
             return False
 
     def addLast(self, item):
+        """
+        Adds an item at the end of Linked list.
+        """
         node = Node(item)
         if self.isEmpty():
             self.first = self.last = node
@@ -29,6 +38,9 @@ class LinkedList:
         self._size += 1
 
     def addFirst(self, item):
+        """
+        Adds an item at the start of Linked list.
+        """
         node = Node(item)
         if self.isEmpty():
             self.first = self.last = node
@@ -39,6 +51,9 @@ class LinkedList:
         self._size += 1
 
     def removeFirst(self):
+        """
+        Removes the first item of linked list.
+        """
         if self.first:
             temp = self.first
             self.first = temp.next
@@ -47,6 +62,9 @@ class LinkedList:
             return -1
 
     def removeLast(self):
+        """
+        Removed the last item of linked list.
+        """
         if self.first:
             current = self.first
             while current.next:
@@ -59,7 +77,10 @@ class LinkedList:
         else:
             return -1
 
-    def indexOf(self,item):
+    def indexOf(self, item):
+        """
+        Returns the index of an item.
+        """
         index = 0
         if self.first:
             current = self.first
@@ -73,16 +94,52 @@ class LinkedList:
         else:
             return -1
 
-    def contains(self,item):
-        if not self.indexOf(item) == -1:
-            return True
-        else:
-            return False
+    def __contains__(self, item) -> bool:
+        """
+        Return if the item contains in list or not.
+        >>> linked_list = LinkedList()
+        >>> 10 in linked_list
+        False
+        >>> linked_list.addFirst(10)
+        >>> linked_list.addLast(20)
+        >>> 10 in linked_list
+        True
+        >>> 30 in linked_list
+        False
+        """
+        return item in self.toArray()
+
+    def __len__(self):
+        """
+        Return length of linked list i.e. number of nodes
+        >>> linked_list = LinkedList()
+        >>> len(linked_list)
+        0
+        >>> linked_list.addLast("tail")
+        >>> len(linked_list)
+        1
+        >>> linked_list.addFirst("head")
+        >>> len(linked_list)
+        2
+        >>> linked_list.removeLast()
+        >>> len(linked_list)
+        1
+        >>> linked_list.removeFirst()
+        >>> len(linked_list)
+        0
+        """
+        return self._size
 
     def size(self):
+        """
+        Returns the size of Linked list.
+        """
         return self._size
 
     def toArray(self):
+        """
+        Converts Linked list to a list.
+        """
         array = []
         if self.first:
             current = self.first
@@ -91,8 +148,46 @@ class LinkedList:
                 current = current.next
         return array
 
+    def reverse(self):
+        """
+        Reverse the linked list.
+        """
+        if self.first:
+            current = self.first
+            prev = None
+            while current:
+                _next = current.next
+                current.next = prev
+                prev = current
+                current = _next
+            self.last = self.first
+            self.first = prev
+        return -1
 
-def main():
+    def getKthNodeFromEnd(self, k):
+        """
+        :param k: int
+        :return: kth element from the last node.
+        """
+        if not self.first:
+            raise EmptyListError("Cannot Search element in empty list.")
+        if k > self.size():
+            raise IndexError("K cannot be greater than the size of linked list.")
+        _last = self.first
+        previous = self.first
+
+        for _ in range(k-1):
+            _last = _last.next
+        while _last != self.last:
+            previous = previous.next
+            _last = _last.next
+        return previous.value
+
+
+class EmptyListError(BaseException):
+    pass
+
+if __name__ == "__main__":
     llist = LinkedList()
     print(llist.isEmpty())
     print(llist.size())
@@ -101,12 +196,9 @@ def main():
     llist.addLast(20)
     llist.addLast(30)
     llist.removeLast()
-    print(llist.size())
-    print(llist.toArray())
-    # print(llist.indexOf(40))
-    # print(llist.contains(40))
+    llist.addLast(40)
+    llist.addLast(50)
+    print(llist.getKthNodeFromEnd(1))
+    # llist.reverse()
     print(llist.isEmpty())
-
-if __name__ == "__main__":
-    main()
-    print("Hello World")
+    print(llist)
